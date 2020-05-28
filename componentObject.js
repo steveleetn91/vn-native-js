82959da538951901826398d847d846fc03d61a9d
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-05-28 15:10:33 +0700

    Update core component object

--- componentObject.js
+++ componentObject.js
@@ -2,17 +2,36 @@ function componentObject () {
     this.create = function(element = "div"){
         this.object = document.createElement(element);
         return this.object;
-    };
+    }
     this.css = function(key,value) {
         this.object.style[key] = value;
-    },
+    }
+    this.cssObject = function(object) {
+        Object.assign(this.object.style,object);
+    }
     this.setEvent = function(key,callback) {
         this.object.addEventListen(key,(e) => {
             callback(e);
         });
     },
-    this.debug = function(){
-        return this.object.toString();
+    this.debug = function(debug = ''){
+        switch(debug) {
+            default :
+                return this.object; 
+            break;
+            case 'style' :
+                return this.object[debug]; 
+            break;
+            case 'id' :
+                return this.object[debug]; 
+            break;
+            case 'class' :
+                return this.object[debug]; 
+            break;
+            case 'text' :
+                return this.object.innerText; 
+            break;
+        }
     },
     this.childComponent = function(object){
         this.object.appendChild(object);
@@ -24,7 +43,7 @@ function componentObject () {
         this.object.setAttribute(key,value);
     }
     this.setText = function(text){
-        this.object.innnerText = text;
+        this.object.innerText = text;
     }
 };
 export default componentObject;
